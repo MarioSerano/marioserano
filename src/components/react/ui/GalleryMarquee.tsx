48205cb0b87c1de2ce5f1b6bb118b9d820abde02
Mario Serano <marioserano55@gmail.com>
2023-08-31 16:02:28 +0700

    chore(about): added docs to calculation for image element count

--- src/components/react/ui/GalleryMarquee.tsx
+++ src/components/react/ui/GalleryMarquee.tsx
@@ -23,6 +23,33 @@ const GalleryMarquee: FC<GalleryMarqueeProps> = (props) => {
 
   const [ref, { width: containerWidth }] = useElementSize();
 
+  /**
+   * MS:
+   * Calculation breakdown
+   * The goal of this calculation is to get the quantity of elements such that if it got translated 100%,
+   * while also being centered in respect of the container, then we still have a full screen.
+   *
+   * Let's take a screen size of 1366px.
+   * So we then have a blank `Formula = 1366px`
+   *
+   * If we just have five images with 360px in width, then we have 1800px in children size.
+   * If it got centered, then we will have a negative margin of 217px. Why?
+   *
+   * Centering formula is 1/2*container - 1/2*width of element. So that we will have 1366/2 - 1800/2 = 683 - 900 = 217px
+   * But then we translated it 100% to the container, we will move it to the right by 1366px.
+   *
+   * Then, we will have 1366px + 217px margin, equals to 1583px of margin
+   * We will then only display 1800px - 1583px of content, equals to 217px of content, while we want this to be 1366px of content.
+   *
+   * So then the formula will be:
+   * Children Width - Container Width (translate 100%) - (1/2*Children Width - 1/2*Container Width) = Container Width
+   *
+   * We can generalize Children Width more:
+   * (#Images * Image Width) - Container Width - (1/2*(#Images * Image Width) - 1/2*Container Width) = Container Width
+   *
+   * Derived, we will then get:
+   * #Images = 3*Container Width/Image Width, which is the `elementCount` calculates.
+   */
   const imagesLength = images.length;
   const elementCount = useMemo(
     () =>

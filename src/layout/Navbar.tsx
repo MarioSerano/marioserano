54ed6f43dcf86e43e0e2e76c8e170d63ce2fc61b
Mario Serano <marioserano55@gmail.com>
2023-12-31 22:37:09 +0700

    feat: add resume

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -34,7 +34,7 @@ const Navbar: FC<NavbarProps> = (props) => {
             <div className="h-6 min-h-[1.5rem] w-[1px] min-w-[1px] bg-white"></div>
           </li>
           <li>
-            <a href="#resume">Resume</a>
+            <a href="https://bit.ly/MarioResumeSE">Resume</a>
           </li>
           <li>
             <a href="#contact">{buttonChildren}</a>
